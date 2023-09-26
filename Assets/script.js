de98632535699322ce5c4479d8b462769fe9f271
Oddux <141483016+Oddux@users.noreply.github.com>
2023-09-25 18:18:59 -0600

    Completed code and readme.

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-  // TODO #1: Add code to display the current date in the header of the page. Done.
-  // TODO #2: Create timeblocks for 9am - 5pm. Done.
-  // TODO #3: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  // TODO #4: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  // TODO #5: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
 var today = dayjs();
 $("#currentDay").text(today.format('dddd MMM D, YYYY'))
 
@@ -40,10 +20,6 @@ $(".time-block").each(function(){
     $(this).addClass("present")
   }
 })
-
-//Reference the save buttons, which on click will write the data written into the timeblocks
-//into local storage. Then add a function that when the page is loaded will call the data
-//into the page.
 
 $(".btn").each(function(){
   $(this).on("click", function(){
